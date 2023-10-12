06a2e6ad12449fcc0236c84c1091bb4b8438c755
lvlinu <s01085056889@gmail.com>
2023-10-12 16:01:13 +0900

    Create 6.cpp

---
+++ Project3/6.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void swap(int* pa, int* pb)
+{
+	int c = *pa;
+	*pa = *pb;
+	*pb = c;
+}
+
+void sort(int arr[], int n)
+{
+
+}
+int main()
+{
+	int arr[] = { 1,3,2,4,5 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	pritnf("")
+}
